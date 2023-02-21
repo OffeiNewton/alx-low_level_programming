f28c05cbc4bb040af02e7dda5d474442626f19c9
OffeiNewton <Solomonwright9@gmail.com>
2023-02-20 17:26:53 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 8 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
 	putchar('\n');
 	return (0);
 }
