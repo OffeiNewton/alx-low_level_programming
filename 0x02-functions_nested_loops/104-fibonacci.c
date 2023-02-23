0f9872faa9c5b6dae5ce6d71eeaf97d12830e37a
OffeiNewton <Solomonwright9@gmail.com>
2023-02-22 22:10:53 -0800

    104 fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, num1 = 1, num2 = 2, next;
+
+	printf("%d, %d, ", num1, num2);
+
+	for (i = 2; i < 98; i++)
+	{
+		next = num1 + num2;
+		printf("%d", next);
+		if (i < 97)
+		{
+			printf(", ");
+		}
+		num1 = num2;
+		num2 = next;
+	}
+	printf("\n");
+	return (0);
+}
