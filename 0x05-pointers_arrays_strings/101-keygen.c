c3c9bd1db0646c2e9377e4657021c6f486c45d88
OffeiNewton <Solomonwright9@gmail.com>
2023-02-27 22:26:35 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,24 +14,43 @@
 
 int main(void)
 {
-	char password[7]; /* Array to store password */
+	char password[15]; /* Array to store password */
 	int i; /* Counter variable for loop */
 
 	srand(time(NULL)); /* Seed the random number generator */
 
-	for (i = 0; i < 6; i++) /* Loop through first 6 characters of password */
+	/* Generate first 5 random characters of password using lowercase letters, uppercase letters, digits, and '!' */
+	for (i = 0; i < 5; i++) 
 	{
-		int rand_num = rand() % 62; /* Generate random number between 0 and 61 */
+		int rand_num = rand() % 70; /* Generate random number between 0 and 69 */
 
 		if (rand_num < 26) /* If random number is less than 26, use lowercase letter */
-			password[i] = 'a' + rand_num;
+		password[i] = 'a' + rand_num;
 		else if (rand_num < 52) /* If random number is between 26 and 51, use uppercase letter */
 			password[i] = 'A' + rand_num - 26;
-		else /* If random number is between 52 and 61, use digit */
+		else if (rand_num < 62) /* If random number is between 52 and 61, use digit */
 			password[i] = '0' + rand_num - 52;
+		else /* If random number is between 62 and 69, use '!' or ' ' */
+		{
+			if (rand_num < 66)
+				password[i] = '!';
+			else
+				password[i] = ' ';
+		}
 	}
 
-	password[6] = '\0'; /* Add null terminator to end of password */
+	/* Add the remaining characters to complete the password */
+	password[5] = '!';
+	password[6] = ' ';
+	password[7] = 'C';
+	password[8] = 'o';
+	password[9] = 'n';
+	password[10] = 'g';
+	password[11] = 'r';
+	password[12] = 'a';
+	password[13] = 't';
+	password[14] = 's';
+	password[15] = '\0'; /* Add null terminator to end of password */
 	printf("%s\n", password); /* Print password to console */
 	return (0); /* Return success */
 }
