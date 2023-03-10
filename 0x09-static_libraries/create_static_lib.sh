64896adbdf5c530d8eef4a09a5ca0e0595c9315d
OffeiNewton <Solomonwright9@gmail.com>
2023-03-10 15:47:39 -0800

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Compile all .c files into object files
 gcc -Wall -Werror -Wextra -pedantic -c *.c
-# Create static library from object files
 ar -rc liball.a *.o
-# Index static library
 ranlib liball.a
-# Remove object files
 rm *.o
