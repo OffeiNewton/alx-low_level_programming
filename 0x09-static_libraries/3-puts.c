4dbe201b0f7db608ebe237486a7264faaf81a81b
OffeiNewton <Solomonwright9@gmail.com>
2023-03-10 15:33:24 -0800

    libmy

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ *
+ * Return: None.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
