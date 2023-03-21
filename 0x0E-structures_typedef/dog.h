7915b2b1065a15438f8f49bc529c21a60ab0dc60
OffeiNewton <Solomonwright9@gmail.com>
2023-03-20 20:30:54 -0700

    4 neew dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,7 @@ typedef struct dog_t
 
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 
 #endif /* DOG_H */
